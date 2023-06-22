cdec59c4b583c934867d6bef6b34bc0514dba2c9
mpinari <mpinari@proveedorexterno.com>
2023-06-22 08:45:16 +0000

    0.0.2

---
+++ models/monitoring_test/sqltest.sql
@@ -0,0 +1,25 @@
+# ../sqltest.sql
+
+# ver 0.0.1
+
+
+
+{{ config(materialized='table') }}
+WITH CTE AS (
+     SELECT
+      name AS nombres,
+      COUNT(name) AS frecuencia,
+        AVG(age) AS media,
+       MIN(age) AS joven,
+       MAX(age) AS mayor
+
+FROM
+{{ source('data','py_data') }}
+
+GROUP BY
+
+ nombres
+
+)
+
+select * from CTE ORDER BY nombres
